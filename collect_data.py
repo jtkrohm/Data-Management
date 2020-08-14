d8a4dd73c96e9d39e3ce55564b5fb40033000e9d
jtkrohm <jtkrohm@github.com>
2020-08-14 17:02:38 -0400

    update collect_data.py

--- collect_data.py
+++ collect_data.py
@@ -1,12 +1,9 @@
-#We will collect data on whether it is day or night based on time
-#in order to build and test our function for making datasets
 import datetime
 import pandas as pd
 from datetime import date
 import time
 
 
-IS_DAY = True
 dayx = {0:"Monday",1:"Tuesday",2:"Wednesday",3:"Thursday",4:"Friday",5:"Saturday",6:"Sunday"}
 day_raw = datetime.datetime.today().weekday()
 today = date.today
