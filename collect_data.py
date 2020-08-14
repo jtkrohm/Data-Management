8522a232819a66060360580f94c040e2ec1adaac
jtkrohm <jtkrohm@github.com>
2020-08-14 16:37:47 -0400

    update coolect_data.py

--- collect_data.py
+++ collect_data.py
@@ -11,20 +11,20 @@ dayx = {0:"Monday",1:"Tuesday",2:"Wednesday",3:"Thursday",4:"Friday",5:"Saturday
 day_raw = datetime.datetime.today().weekday()
 today = date.today
 x = ["Time","Day", "Month","Year"]
-print(dayx[day_raw])
+$print(dayx[day_raw])
+print(x)
 m = datetime.datetime.today().month
 y = datetime.datetime.today().year
 t = str(datetime.datetime.now().time())
 data = [x]
 
 
-while True: 
+while True:
 	today = date.today
-	day_raw = datetime.datetime.today().weekday() 
-	m = datetime.datetime.today().month 
+	day_raw = datetime.datetime.today().weekday()
+	m = datetime.datetime.today().month
 	y = datetime.datetime.today().year
 	t = str(datetime.datetime.now().time())
-	data.append([t, dayx[day_raw], m, y]) 
-        print(data[-1])
-	time.sleep(15) 
- 
+	data.append([t, dayx[day_raw], m, y])
+	print(data[-1])
+	time.sleep(15)
